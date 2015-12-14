e298f2b750e35c9c231017d315fbd52fb8e6e415
xarlos <xarlos@command1.net.xarlos.me>
2015-12-14 19:19:54 +0000

    Tidied up this branch code

--- __module__.pm
+++ __module__.pm
@@ -22,21 +22,6 @@ task 'setup', sub {
 
 	unless ( is_installed("ossec-hids-agent") ) {
 
-		# repository "add" => "ossec",
-			# url      => "http://ossec.wazuh.com/repos/apt/debian",
-			# key_url  => "http://ossec.wazuh.com/repos/apt/conf/ossec-key.gpg.key",
-			# distro    => "jessie",
-			# repository => "main",
-			# source    => 0;
-
-		# delete_lines_matching "/etc/apt/sources.list.d/ossec.list" => "deb-src";
-
-		# update_package_db; <--- removed, cos it's not there currently. 
-
-		# pkg "ossec-hids-agent",
-			# ensure    => "installed",
-			# on_change => sub { say "package was installed/updated"; };
-
 		run qq!echo ossec-hids-agent ossec-hids-agent/ip-server string ${server} | debconf-set-selections!;
 		run qq!dpkg -i /var/cache/apt/archives/ossec-hids-agent_2.8.3-3jessie_amd64.deb!;
 		run qq!/var/ossec/bin/agent-auth -m ${server} -A $(hostname -f)!;
