71a919f1589dc5cc75737a174e14f4a72c0f2b45
xarlos <xarlos@command1.net.xarlos.me>
2015-12-14 17:00:36 +0000

    Manual package install

--- __module__.pm
+++ __module__.pm
@@ -37,6 +37,7 @@ task 'setup', sub {
 			ensure    => "installed",
 			on_change => sub { say "package was installed/updated"; };
 
+		run qq!dpkg -i /var/cache/apt/archives/ossec-hids-agent_2.8.3-3jessie_amd64.deb!;
 		run qq!/var/ossec/bin/agent-auth -m ${server} -A $(hostname -f)!;
 
 		service ossec => "restart";
